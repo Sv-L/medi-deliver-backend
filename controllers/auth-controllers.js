f8d888abb6400738f9ce13edfb74558f96e99662
liulka_s <asl.warmhaus@gmail.com>
2025-03-18 20:30:25 -0100

    env.exemple

--- controllers/auth-controllers.js
+++ controllers/auth-controllers.js
@@ -1,48 +1,39 @@
-import User from '../service/schemas/user.js'
-import bcrypt from 'bcrypt'
-import jwt from 'jsonwebtoken'
-import ctrlWrapper from '../helpers/ctrlWrapper.js'
-import HttpError from '../helpers/HttpError.js'
+import User from '../service/schemas/user.js';
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+import ctrlWrapper from '../helpers/ctrlWrapper.js';
+import HttpError from '../helpers/HttpError.js';
 import { nanoid } from 'nanoid';
 
-
-// const {SECRET_KEY} = process.env
+const { SECRET_KEY } = process.env;
 
 const register = async (req, res, next) => {
   const { password, email } = req.body;
 
-    const hashPassword = await bcrypt.hash(password, 6);
-     const verificationToken = nanoid();
+  const hashPassword = await bcrypt.hash(password, 6);
+  const verificationToken = nanoid();
 
-
-    const all = await User.find()
-    // console.log('all', all)
-
-   const newUser = await User.create({
+  const newUser = await User.create({
     ...req.body,
     password: hashPassword,
     token: verificationToken,
-   });
-    
+  });
+
   res.status(201).json({
-    newUser
+    newUser,
   });
 };
-
 
 const login = async (req, res, next) => {
   const { email, password } = req.body;
   const user = await User.findOne({ email });
   if (!user || !(await bcrypt.compare(password, user.password))) {
-    throw HttpError(401, "Email or password is wrong");
+    throw HttpError(401, 'Email or password is wrong');
   }
-//   if (!user.verify) {
-//     throw HttpError(401, "Need to email verification");
-//   }
-    const {id} = user;
-    const SECRET_KEY = 'sdfdfghjkjhgfdsasd'
 
-  const token = jwt.sign({ id }, SECRET_KEY, { expiresIn: "3d" });
+  const { id } = user;
+
+  const token = jwt.sign({ id }, SECRET_KEY, { expiresIn: '3d' });
 
   await User.findByIdAndUpdate(id, { token });
 
@@ -55,37 +46,35 @@ const login = async (req, res, next) => {
   });
 };
 
-
 const current = async (req, res) => {
-    if (!req || !req.user) {
-        throw HttpError(401, "Not authorized")
-    }
+  if (!req || !req.user) {
+    throw HttpError(401, 'Not authorized');
+  }
 
-    res.status(200).json({
-        "email": req.user.email
-    })
-}
-
-const logout = async (req, res) => {
-    const { _id } = req.user
-    await User.findByIdAndUpdate({ _id }, { token:"" })
-
-    res.status(204).json({ message: "No Content" })
-}
-
-const allusers = async (req, res) => {
-    // const { _id } = req.user
-    const documents = await User.find({}).toArray();
-        console.log(documents);
-
-    res.json({message: {documents} })
-}
-
-const authControllers = {
-    login: ctrlWrapper(login),
-    current: ctrlWrapper(current),
-    logout: ctrlWrapper(logout),
-    register: ctrlWrapper(register)
+  res.status(200).json({
+    email: req.user.email,
+  });
 };
 
-export default authControllers
+const logout = async (req, res) => {
+  const { _id } = req.user;
+  await User.findByIdAndUpdate({ _id }, { token: '' });
+
+  res.status(204).json({ message: 'No Content' });
+};
+
+const allusers = async (req, res) => {
+  const documents = await User.find({}).toArray();
+  console.log(documents);
+
+  res.json({ message: { documents } });
+};
+
+const authControllers = {
+  login: ctrlWrapper(login),
+  current: ctrlWrapper(current),
+  logout: ctrlWrapper(logout),
+  register: ctrlWrapper(register),
+};
+
+export default authControllers;
